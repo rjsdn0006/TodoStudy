33db6766808ef6bcb4134e9a625dbc7eeeab6936
Kim Gunwoo <80525718+rjsdn0006@users.noreply.github.com>
2022-05-28 11:14:37 +0900

    백엔드구현중

---
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // Getter Setter 통합생성
+@Entity
+@Table(name = "Todo")
+
+public class TodoEntity {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	private String id;
+	
+	private String userId;
+	private String title;
+	private boolean done;
+}
